fbd2058f8d14dccd704e7379b4ab3ccafb5230da
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-04-02 13:47:51 +0300

    Added a patch request

--- script.js
+++ script.js
@@ -1,76 +1,67 @@
-// function movies(){
-//     fetch("http://localhost:3000/films")
-//     .then(res=>res.json());
-//     .then(data=>console.log(data));
-//     //console.log('movie');
-//     let moviesData=data;
-
-//     for (let i = 0; i < moviesData; i++) {
-// 	console.log(moviesData[i].tickets_sold);
-
-//     let movieId=document.querySelector("movieId");
-//     let availableMovies=document.querySelector("availableMovies");
-//     let movieTitle=document.querySelector(" movieTitle");
-//     let movieRuntime=document.querySelector("movieRuntime");
-//     let movieCapacity=document.querySelector("movieCapacity");
-//     let movieShowtime=document.querySelector("movieShowtime");
-//     let movieTicketsSold=document.querySelector("movieTicketsSold");
-//     let movieDescription=document.querySelector("movieDescription");
-//     let moviePoster=document.querySelector("moviePoster");
-//     let movieShowingId=document.querySelector("movieShowingId");
-
-//     function availableMovies(){
-//         let availableMovies=document.querySelector(".availableMovies").value;
-//         document.querySelector(".availableMovies").innerHTML=availableMovies;
-//     }
-// }
-// };
-function movies() {
-  fetch("http://localhost:3000/films")
+function movie() {
+  fetch(`http://localhost:3000/films`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      // do something with the data here
-      let moviesData = data;
-
-      for (let i = 0; i < moviesData.length; i++) {
-        console.log(moviesData[i].tickets_sold);
-
-        // use # to select an element by ID
-        let movieId = document.querySelector("#movieId");
-        let availableMovies = document.querySelector("#availableMovies");
-        let movieTitle = document.querySelector("#movieTitle");
-        let movieRuntime = document.querySelector("#movieRuntime");
-        let movieCapacity = document.querySelector("#movieCapacity");
-        let movieShowtime = document.querySelector("#movieShowtime");
-        let movieTicketsSold = document.querySelector("#movieTicketsSold");
-        let movieDescription = document.querySelector("#movieDescription");
-        let moviePoster = document.querySelector("#moviePoster");
-        let movieShowingId = document.querySelector("#movieShowingId");
-
-        movieTitle.textContent = moviesData[0].title;
-        moviePoster.src = moviesData[0].poster;
-        movieRuntime.textContent = moviesData[0].runtime;
-        movieCapacity.textContent = moviesData[0].capacity;
-        movieShowtime.textContent = moviesData[0].showtime;
-        movieTicketsSold.textContent = moviesData[0].tickets_sold;
-
-        availableMovies.appendChild(title);
-        availableMovies.appendChild(poster);
-        availableMovies.appendChild(runtime);
-        availableMovies.appendChild(capacity);
-        availableMovies.appendChild(showtime);
-        availableMovies.appendChild(tickets_sold);
-        availableMovies.appendChild(description);
-        
-
-        // define the availableMovies function outside of the loop
-        function remainingMovies() {
-          let availableMovies =
-            document.querySelector("#availableMovies").value;
-          document.querySelector("#availableMovies").innerHTML =
-            availableMovies;
-        }
+      const movieData = data;
+      const filmsList = document.querySelector("#films");
+      for (let i = 0; i < movieData.length; i++) {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.href = "#";
+        a.textContent = movieData[i].title;
+        li.appendChild(a);
+        filmsList.appendChild(li);
+        a.addEventListener("click", () => {
+          const display = document.querySelector("#display");
+          display.innerHTML = "";
+          const title = document.createElement("h1");
+          const poster = document.createElement("img");
+          const description = document.createElement("h3");
+          const showTime = document.createElement("h4");
+          const runtime = document.createElement("h4");
+          const tickets = document.createElement("h4");
+          const buy = document.createElement("button");
+          title.textContent = movieData[i].title;
+          poster.src = movieData[i].poster;
+          description.textContent = `Description: ${movieData[i].description}`;
+          showTime.textContent = `Movie starts at: ${movieData[i].showtime}`;
+          runtime.textContent = `Duration: ${movieData[i].runtime} minutes`;
+          tickets.textContent = `Tickets remaining: ${
+            movieData[i].capacity - movieData[i].tickets_sold
+          }`;
+          buy.textContent = `BUY TICKET`;
+          display.appendChild(title);
+          display.appendChild(poster);
+          display.appendChild(description);
+          display.appendChild(showTime);
+          display.appendChild(runtime);
+          display.appendChild(buy);
+          display.appendChild(tickets);
+          buy.addEventListener("click", (e) => {
+            e.preventDefault();
+            movieData[i].tickets_sold++;
+            const remainingTickets =
+              movieData[i].capacity - movieData[i].tickets_sold;
+            fetch(`http://localhost:3000/films/${movieData[i].id}`, {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                tickets_sold: movieData[i].tickets_sold,
+              }),
+            })
+              .then((response) => {
+                if (response.ok) {
+                  tickets.textContent = `Tickets remaining: ${remainingTickets}`;
+                }
+              })
+              .catch((err) => console.log(err));
+            tickets.textContent = `Tickets remaining: ${remainingTickets}`;
+          });
+        });
       }
-    });
+    })
+    .catch((err) => console.log(err));
 }
+movie();
